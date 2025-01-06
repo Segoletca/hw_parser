c393bec1f071692e2ffadb2cc365e39a7644a256
Segoletca <sergey.noskov.2001@mail.ru>
2025-01-06 19:31:41 +0300

    Edit indent in logging

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def configure_logging():
     logging.basicConfig(
         level=logging.DEBUG,
         datefmt="%Y-%m-%d %H:%M:%S",
-        format="[%(asctime)s.%(msecs)03d] %(module)40s:%(lineno)-3d %(levelname)-7s - %(message)s",
+        format="[%(asctime)s.%(msecs)03d] %(module)40s:%(lineno)-4d %(levelname)-7s - %(message)s",
         # filename="legobase.log",
         # filemode="w",
     )
