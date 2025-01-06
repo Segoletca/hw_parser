7f35eca3d80b548a96732e5bd4f2b37d7393cc55
Segoletca <sergey.noskov.2001@mail.ru>
2025-01-06 20:18:57 +0300

    Add method for save headers in json

--- Parser.py
+++ Parser.py
@@ -1,13 +1,50 @@
 import requests as re
+from http import HTTPStatus
+from logging import getLogger
+import json
+from config import Paths
 
+
+log = getLogger(__name__)
+
+"""_summary_
+
+Raises:
+    SystemExit: _description_
+"""
 
 class Parser:
     def __init__(self, url):
+        self.savingObj = None
+        self.fileName = None
         self.url = url
         self.page = re.get(self.url)
+        if self.page.status_code == 404:
+            log.warning("Ошибка get запроса!")
+            log.error("Не найдено! Возможно URL указан неверно.")
+            raise SystemExit
+        # if page == re.models.Response
+            
         self.headers = self.page.headers
         # self.test = self.page.json
         
     def res(self):
-        # print(self.headers)
-        print(self.headers)
+        print()
+
+        
+    def saveJson(self):
+        # Needs: - fileName and savingObj
+
+        with open(f"{Paths.DATA_PATH}/{self.fileName}", "w") as file:
+            json.dump(self.savingObj, file, indent=4)
+
+    def saveHeader(self):
+        # In json format
+        self.fileName = "raw_head_out.json"
+        self.savingObj = dict(self.headers)
+        
+        self.saveJson()
+        log.info(f"Файл {self.fileName} был сохранен.")
+        
+        self.fileName, self.savingObj = None, None
+        
--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import logging
 from config import configure_logging
 from Parser import Parser
 
-log = logging.getLogger(__name__)
-
-url = "https://habr.com/ru/companies/ruvds/articles/665084/"
-
-parser = Parser(url)
 
 def main():
+    log = logging.getLogger(__name__)
+    log.info("Start program!")
+    
+    url = "https://habr.com/ru/companies/ruvds/articles/665084/"
+    
+    parser = Parser(url)
     parser.res()
+    
+    # parser.saveHeader()
 
 
 
 if __name__ == '__main__':
     configure_logging()
-    log.info("Start program!")
     main()
