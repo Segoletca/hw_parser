0de81348a8a7e6b00282c336ffe8deb8a7201295
Segoletca <sergey.noskov.2001@mail.ru>
2025-01-08 21:02:04 +0300

    Update Parser class

--- Parser.py
+++ Parser.py
@@ -21,20 +21,20 @@ class Parser:
         self.url = url
         self.base_url = url
         self.data = data
-        self.page = re.get(self.url, self.data)
-        if self.page.status_code == 404:
+        self.response = re.get(self.url, self.data)
+        if self.response.status_code == 404:
             log.warning("Ошибка get запроса!")
             log.error("Не найдено! Возможно URL указан неверно.")
             raise SystemExit
-        # if page == re.models.Response
+        # if response == re.models.Response
             
-        self.headers = self.page.headers
-        # self.test = self.page.json
-   
-   
-    def getPageNoExept(self):
-        self.page = re.get(self.url, self.data)
-   
+        self.headers = self.response.headers
+        # self.test = self.response.json
+
+
+    def getResponseNoExept(self):
+        self.response = re.get(self.url, self.data)
+
     def saveJson(self):
         # Needs: - fileName and savingObj
         with open(f"{Paths.DATA_PATH}/{self.fileName}", "w") as file:
@@ -49,16 +49,16 @@ class Parser:
         log.info(f"Файл {self.fileName} был сохранен.")
         
         self.fileName, self.savingObj = None, None
-        
+
     def saveBody(self):
         self.fileName = "raw_body_out.json"
-        self.savingObj = dict(self.page.text)
+        self.savingObj = dict(self.response.text)
         
         self.saveJson()
         log.info(f"Файл {self.fileName} был сохранен.")
-    
+
     # def res(self):
     #     # with open(f"{Paths.DATA_PATH}/raw_body_out.json", "w") as file:
-    #     #     file.write(self.page.text)
+    #     #     file.write(self.response.text)
     #     self.saveHeader()
     #     print()
